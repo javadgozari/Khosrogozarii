99a9259062d9a056e3a6ea774309ffb6889f6e29
javadgozari <javadgozari88@gmail.com>
2025-06-08 16:09:39 +0330

    Update emotion_brain.py

--- modules/emotion_brain.py
+++ modules/emotion_brain.py
@@ -1,3 +1,12 @@
 class EmotionBrain:
+    def __init__(self, memory=None):
+        self.memory = memory
+
     def detect(self, message):
-        return 'خنثی'
+        emotion = 'خنثی'
+        if self.memory:
+            self.memory.save("emotion", f"{message} => {emotion}")
+        return emotion
+
+    def activate(self):
+        print("❤️ مغز احساس فعال شد.")
