5e4a936aff9aa12475edf60ddf140ff5d024788e
javadgozari <javadgozari88@gmail.com>
2025-06-08 16:11:40 +0330

    Update emotion_responder.py

--- modules/emotion_responder.py
+++ modules/emotion_responder.py
@@ -1,3 +1,12 @@
 class EmotionResponder:
+    def __init__(self, memory=None):
+        self.memory = memory
+
     def respond(self, mood):
-        return 'می‌فهمم چه حسی داری...'
+        response = f"می‌فهمم چه حسی داری... ({mood})"
+        if self.memory:
+            self.memory.save("emotion_response", response)
+        return response
+
+    def activate(self):
+        print("💬 مغز پاسخ‌دهنده احساسی فعال شد.")
