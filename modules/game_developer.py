95cf0d486f5a352ad848c500160a0c84f6d0b8bf
javadgozari <javadgozari88@gmail.com>
2025-06-08 16:12:35 +0330

    Update game_developer.py

--- modules/game_developer.py
+++ modules/game_developer.py
@@ -1,3 +1,19 @@
 class GameDeveloper:
+    def __init__(self, memory=None):
+        self.memory = memory
+        self.game_history = []
+
     def create_game(self, idea):
-        return f'یک بازی برای کیمیا ساخته می‌شود با موضوع: {idea}'
+        log = f"یک بازی برای کیمیا ساخته می‌شود با موضوع: {idea}"
+        self.game_history.append({"idea": idea, "log": log})
+        if self.memory:
+            self.memory.save("game_idea", idea)
+        return log
+
+    def get_last_game(self):
+        if self.game_history:
+            return self.game_history[-1]['log']
+        return "هنوز هیچ بازی‌ای ساخته نشده است."
+
+    def activate(self):
+        print("🎮 مغز توسعه‌دهنده بازی فعال شد.")
