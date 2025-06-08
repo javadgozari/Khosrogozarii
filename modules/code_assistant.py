e3dc79cf581433d32e709e8ca1baf645775f33e9
javadgozari <javadgozari88@gmail.com>
2025-06-08 16:05:59 +0330

    Update code_assistant.py

--- modules/code_assistant.py
+++ modules/code_assistant.py
@@ -1,3 +1,12 @@
 class CodeAssistant:
+    def __init__(self, memory=None):
+        self.memory = memory
+
     def help(self, query):
-        return 'پیشنهاد کد پایتون: print("سلام")'
+        suggestion = 'پیشنهاد کد پایتون: print("سلام")'
+        if self.memory:
+            self.memory.save("code_help", f"درخواست: {query} => پاسخ: {suggestion}")
+        return suggestion
+
+    def activate(self):
+        print("🤖 دستیار کدنویسی فعال شد.")
