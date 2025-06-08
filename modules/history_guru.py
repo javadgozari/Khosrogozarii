1338286368fa3e6d004517ff3bb1c1834e281e24
javadgozari <javadgozari88@gmail.com>
2025-06-08 16:14:37 +0330

    Update history_guru.py

--- modules/history_guru.py
+++ modules/history_guru.py
@@ -1,3 +1,17 @@
+import logging
+
 class HistoryGuru:
+    def __init__(self):
+        self.logger = logging.getLogger("HistoryGuru")
+        self.logger.setLevel(logging.INFO)
+        if not self.logger.hasHandlers():
+            handler = logging.StreamHandler()
+            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            handler.setFormatter(formatter)
+            self.logger.addHandler(handler)
+        self.logger.info("📜 مغز تاریخ فعال شد.")
+
     def fact(self):
-        return 'کوروش بزرگ نخستین منشور حقوق بشر را نوشت.'
+        fact_text = 'کوروش بزرگ نخستین منشور حقوق بشر را نوشت.'
+        self.logger.info(f"📚 داده تاریخی ارائه شد: {fact_text}")
+        return fact_text
