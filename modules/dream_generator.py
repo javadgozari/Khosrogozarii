1bce0997ba4fb5123c3ce7a0757120f8ba386f69
javadgozari <javadgozari88@gmail.com>
2025-06-08 16:08:42 +0330

    Update dream_generator.py

--- modules/dream_generator.py
+++ modules/dream_generator.py
@@ -1,3 +1,12 @@
 class DreamGenerator:
+    def __init__(self, memory=None):
+        self.memory = memory
+
     def generate(self):
-        return 'یک ایده جدید: رباتی که نقاشی می‌کشد!'
+        idea = 'یک ایده جدید: رباتی که نقاشی می‌کشد!'
+        if self.memory:
+            self.memory.save("dream", idea)
+        return idea
+
+    def activate(self):
+        print("💡 مغز تولید رویا فعال شد.")
