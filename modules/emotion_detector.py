e5db0ad627b53ca627289d73b0a7b6be340f24b2
javadgozari <javadgozari88@gmail.com>
2025-06-08 16:10:47 +0330

    Update emotion_detector.py

--- modules/emotion_detector.py
+++ modules/emotion_detector.py
@@ -1,3 +1,12 @@
 class EmotionDetector:
+    def __init__(self, memory=None):
+        self.memory = memory
+
     def detect(self, voice):
-        return 'احساس شناسایی‌شده: غم آمیخته با امید ❤️'
+        detected_emotion = 'احساس شناسایی‌شده: غم آمیخته با امید ❤️'
+        if self.memory:
+            self.memory.save("emotion_detected", f"{voice} => {detected_emotion}")
+        return detected_emotion
+
+    def activate(self):
+        print("🎙️ مغز تشخیص احساس از صدا فعال شد.")
