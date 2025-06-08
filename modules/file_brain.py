a3b5b74b44ed98bc89ee5a04059adb0ffbf98e8a
javadgozari <javadgozari88@gmail.com>
2025-06-08 18:16:54 +0330

    Create file_brain.py

---
+++ modules/file_brain.py
@@ -0,0 +1,6 @@
+class FileBrain:
+    def read(self, filename):
+        return f"محتوای فایل {filename} خوانده شد."
+
+    def write(self, filename, content):
+        return f"محتوا در {filename} ذخیره شد."
