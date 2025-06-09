b5cf483a450e0bc70529c42a1c01d0625b0835db
javadgozari <javadgozari88@gmail.com>
2025-06-09 12:41:56 +0330

    Create Build.sh

---
+++ Build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# ارتقای pip
+pip install --upgrade pip
+
+# نصب پکیج‌ها به صورت دستی
+pip install openai==1.0.0
+pip install transformers==4.39.3
+pip install torch==2.0.1
+pip install sentencepiece
+pip install pydub speechrecognition librosa
+pip install pytesseract opencv-python Pillow
+pip install googletrans==4.0.0rc1 langdetect
+pip install python-telegram-bot==20.3
+pip install python-dotenv requests aiohttp
+pip install tqdm scikit-learn nltk numpy matplotlib beautifulsoup4
